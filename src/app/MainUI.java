dca654ec090850aa7f2a165e30fe71202d7d7b19
User <User@10.216.5.175>
2024-09-04 09:09:56 -0500

    Ejemplo

---
+++ src/app/MainUI.java
@@ -0,0 +1,12 @@
+
+package app;
+
+public class MainUI {
+
+    public static void main(String[] args) {
+       
+        
+        
+    }
+    
+}
---
+++ src/model/cliente.java
@@ -0,0 +1,10 @@
+
+package model;
+
+public class cliente {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
