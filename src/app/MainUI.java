ff1b6377fe3f7349f64d896a10f7a2326108e9dc
Claudio-UTP <u22216115@utp.edu.pe>
2024-09-04 09:24:53 -0500

    +Adios Mundo

--- src/app/MainUI.java
+++ src/app/MainUI.java
@@ -6,8 +6,8 @@ public class MainUI {
     public static void main(String[] args) {
        
         System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo");
+        System.out.println("Tardes Mundo");
+        System.out.println("Adios Mundo");
         
     }
     
